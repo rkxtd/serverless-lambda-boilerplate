ff82b05bb1dd820e5a6998f6a4d1c98afe5e6c9f
Roman Shuvaryk <roman_shuvaryk@epam.com>
2017-03-23 10:17:20 +0200

    * Fixed trailing dot in APIDOC url

--- scripts/generateApidocConfig.js
+++ scripts/generateApidocConfig.js
@@ -5,7 +5,7 @@ var pjson = require('../package.json');
 var config = require('../src/config');
 var serviceURLBeginning = fs.readFileSync('./build/.serviceurl');
 var serverlessYAML = YAML.load('./build/serverless.yml');
-var serviceURL = serviceURLBeginning.toString().replace(/\n$/, "") + '.' + serverlessYAML.provider.region + '.' + '.amazonaws.com/' + serverlessYAML.provider.stage;
+var serviceURL = serviceURLBeginning.toString().replace(/\n$/, "") + '.' + serverlessYAML.provider.region + '.amazonaws.com/' + serverlessYAML.provider.stage;
 
 const apiDocConfig = {
     "name": config.apidoc.name,
