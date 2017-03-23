07deef0edd77fb50ec39927fcf6703fedbf4f976
Roman Shuvaryk <roman_shuvaryk@epam.com>
2017-03-23 10:15:32 +0200

    + Added possibility to generate APIDOC based on URL of the created stack

--- scripts/generateApidocConfig.js
+++ scripts/generateApidocConfig.js
@@ -1,13 +1,18 @@
-var pjson = require('../package.json');
 var jsonfile = require('jsonfile');
+var YAML = require('yamljs');
+var fs = require('fs');
+var pjson = require('../package.json');
 var config = require('../src/config');
+var serviceURLBeginning = fs.readFileSync('./build/.serviceurl');
+var serverlessYAML = YAML.load('./build/serverless.yml');
+var serviceURL = serviceURLBeginning.toString().replace(/\n$/, "") + '.' + serverlessYAML.provider.region + '.' + '.amazonaws.com/' + serverlessYAML.provider.stage;
 
 const apiDocConfig = {
     "name": config.apidoc.name,
     "version": pjson.version,
     "description": config.apidoc.desc,
-    "title": config.apidoc.title,
-    "url" : config.aws.url
+    "title": config.apidoc.title + ' v[' +serverlessYAML.provider.stage + ']',
+    "url" : serviceURL
 };
 
 jsonfile.writeFileSync('./apidoc.json', apiDocConfig);
