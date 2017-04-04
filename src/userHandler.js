a88765e027e868476bc786cc5f67657b1b616199
Roman Shuvaryk <roman_shuvaryk@epam.com>
2017-04-04 16:51:54 +0300

    * Updated documentation to new version

--- src/userHandler.js
+++ src/userHandler.js
@@ -13,6 +13,7 @@ const UsersDTO = new Table('aws/s3', 'users');
  *
  * @apiSuccess {Array} results List of the user entities
  * @apiSuccess {Number} total number of user records
+ *
  * @apiError {Number} statusCode ErrorCode
  * @apiError {String} error Error Message
  */
@@ -44,6 +45,9 @@ module.exports.users = (event, context, callback) => {
  * @apiSuccess {String} firstName First Name of the user
  * @apiSuccess {String} lastName Last Name of the user
  * @apiSuccess {Number} age Age of the user
+ *
+ * @apiError {Number} statusCode ErrorCode
+ * @apiError {String} error Error Message
  */
 module.exports.user = (event, context, callback) => {
     const userId = event.queryStringParameters.id;
@@ -80,18 +84,17 @@ module.exports.user = (event, context, callback) => {
  * @apiName UserPost
  * @apiGroup User
  *
- * @apiVersion 0.1.0
+ * @apiVersion 0.5.0
  *
  * @apiParam {String} firstName User First Name.
  * @apiParam {String} lastName User Last Name.
  * @apiParam {Number} [age] Age of the user
  *
- * @apiSuccess {String} id  Just created user id.
- * @apiSuccess {String} firstName User First Name.
- * @apiSuccess {String} lastName User Last Name.
- * @apiSuccess {Number} age Age of the user
+ * @apiSuccess {Array} results List of the user entities
+ * @apiSuccess {Number} total number of user records
  *
- * @apiError {String} error Error Message.
+ * @apiError {Number} statusCode ErrorCode
+ * @apiError {String} error Error Message
  */
 module.exports.create = (event, context, callback) => {
     const params = querystring.decode(event.body);
@@ -141,20 +144,18 @@ module.exports.create = (event, context, callback) => {
  * @apiName userPut
  * @apiGroup User
  *
- * @apiVersion 0.1.0
+ * @apiVersion 0.5.0
  *
  * @apiParam {String} id Existing user unique ID.
  * @apiParam {String} [firstName] User First Name.
  * @apiParam {String} [lastName] User Last Name.
  * @apiParam {String} [age] Age of the user
  *
- * @apiSuccess {String} id  User Id.
- * @apiSuccess {String} firstName User First Name.
- * @apiSuccess {String} lastName User Last Name.
- * @apiSuccess {Number} age Age of the user
+ * @apiSuccess {Array} results List of the user entities
+ * @apiSuccess {Number} total number of user records
  *
- * @apiError {Number} statusCode ErrorCode.
- * @apiError {String} error Error Message.
+ * @apiError {Number} statusCode ErrorCode
+ * @apiError {String} error Error Message
  */
 module.exports.update = (event, context, callback) => {
     const queryParams = event.queryStringParameters || event;
@@ -216,17 +217,15 @@ module.exports.update = (event, context, callback) => {
  * @apiName userDelete
  * @apiGroup User
  *
- * @apiVersion 0.2.0
+ * @apiVersion 0.5.0
  *
  * @apiParam {Number} id Existing user unique ID.
  *
- * @apiSuccess {String} id  User Id.
- * @apiSuccess {String} firstName User First Name.
- * @apiSuccess {String} lastName User Last Name.
- * @apiSuccess {Number} age Age of the user
+ * @apiSuccess {Array} results List of the user entities
+ * @apiSuccess {Number} total number of user records
  *
- * @apiError {Number} statusCode ErrorCode.
- * @apiError {String} error Error Message.
+ * @apiError {Number} statusCode ErrorCode
+ * @apiError {String} error Error Message
  */
 module.exports.delete = (event, context, callback) => {
     const queryParams = event.queryStringParameters || event;
